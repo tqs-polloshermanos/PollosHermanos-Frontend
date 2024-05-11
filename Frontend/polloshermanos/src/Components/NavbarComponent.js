7be9c4a6d541cc4931a090e0645ee0ee553f518b
Joana <joanaagomes@ua.pt>
2024-05-11 16:14:46 +0100

    init

---
+++ Frontend/polloshermanos/src/Components/NavbarComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NavbarComponent.css'; // Import CSS file
+
+function NavbarComponent() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <a href="#home" className="brand">Pollos Hermanos</a>
+          <ul className="nav-list">
+            <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="#menu" className="nav-link">Menu</a></li>
+            <li className="nav-item"><a href="#locations" className="nav-link">Locations</a></li>
+            <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+          </ul>
+          <a href="#login" className="login-btn nav-link">Login/Sign-up</a>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavbarComponent;
