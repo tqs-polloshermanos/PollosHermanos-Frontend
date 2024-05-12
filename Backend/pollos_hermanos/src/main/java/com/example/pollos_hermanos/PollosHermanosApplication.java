3320b1906b58831d03f95591685744f7d3661490
Maria Joao <mariasardinha@ua.pt>
2024-05-12 20:49:01 +0100

    spring boot init

---
+++ Backend/pollos_hermanos/src/main/java/com/example/pollos_hermanos/PollosHermanosApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollos_hermanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollosHermanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollosHermanosApplication.class, args);
+	}
+
+}
---
+++ Backend/pollos_hermanos/src/test/java/com/example/pollos_hermanos/PollosHermanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pollos_hermanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollosHermanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
