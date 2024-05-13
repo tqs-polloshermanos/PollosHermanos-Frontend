a7bcc0ac31787117de4c53d4f94e75f24d0b107a
Maria Joao <mariasardinha@ua.pt>
2024-05-13 10:59:22 +0100

    spring boot now working

--- Backend/polloshermanos/src/main/java/com/example/polloshermanos/PolloshermanosApplication.java
+++ Backend/polloshermanos/src/main/java/com/example/polloshermanos/PolloshermanosApplication.java
@@ -1,13 +1,13 @@
-package com.example.pollos_hermanos;
+package com.example.polloshermanos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PollosHermanosApplication {
+public class PolloshermanosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PollosHermanosApplication.class, args);
+		SpringApplication.run(PolloshermanosApplication.class, args);
 	}
 
 }
---
+++ Backend/polloshermanos/src/test/java/com/example/polloshermanos/PolloshermanosApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.example.polloshermanos;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class PolloshermanosApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
